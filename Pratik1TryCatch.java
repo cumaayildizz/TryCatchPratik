85902fdc2f815e9d1f8ffe3ad5c16cd2e2f07e32
Cuma YILDIZ <142337560+cumaayildizz@users.noreply.github.com>
2023-10-08 19:32:48 +0300

    change
    change

--- Pratik1TryCatch.java
+++ Pratik1TryCatch.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
+
 public class Pratik1TryCatch {
 
-    public static void CheckIndex(int index) throws IndexCheckException{
-        if(index > 9 || index < 0){
+    public static void CheckIndex(int arr[] , int index) throws IndexCheckException{
+        if(index > arr.length-1 || index < 0){
             throw new IndexCheckException("Hata!!");
         }else {
             System.out.println("Girilen index dizi boyutlari icerisindedir.");
@@ -17,14 +18,14 @@ public class Pratik1TryCatch {
 
         int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        System.out.println("10 elemanli dizimizdeki elemanlardan gormek istediginiz elemanin indexini giriniz");
+        System.out.println(numbers.length + " elemanli dizimizdeki elemanlardan gormek istediginiz elemanin indexini giriniz");
         System.out.println();
 
         System.out.print("Index : ");
         int index = scanner.nextInt();
 
         try {
-             CheckIndex(index);
+             CheckIndex(numbers , index);
 
         }catch (IndexCheckException e){
             e.toString();
